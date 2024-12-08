5e598d6a7a40d8ef296913a1f2d6164a8b245e41
Mihai Andrei <andreimihai2305is@gmail.com>
2024-12-08 13:23:32 +0200

    added cors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const corsOptions_1 = __importDefault(require("../config/corsOptions"));
 const logger_1 = __importDefault(require("../middleware/logger"));
+dotenv_1.default.config();
 const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)(corsOptions_1.default));
 app.use(logger_1.default);
 app.get("/", (req, res) => {
     res.send("Hello, World!").status(200);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import corsOptions from "../config/corsOptions";
 import logReq from "../middleware/logger";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app : Express = express();
+
+app.use(cors(corsOptions));
 app.use(logReq);
 
 app.get("/", (req: Request, res: Response) => {
