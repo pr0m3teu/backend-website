5287c62fc416892bfdb7c921fc85a044fe62935f
Mihai Andrei <andreimihai2305is@gmail.com>
2024-12-12 18:32:05 +0200

    finished products for now

--- routes/productsRouter.ts
+++ routes/productsRouter.ts
@@ -7,7 +7,7 @@ const productsRouter: Router = Router();
 // GET
 productsRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
      try {
-          const products: ProductInterface[] = await Product.find({});
+          const products: ProductInterface[] = await Product.find({}).lean();
           res.status(200).json(products);
      } 
      catch (err : any)
@@ -20,28 +20,33 @@ productsRouter.get("/", async (req: Request, res: Response, next: NextFunction) 
 
 productsRouter.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
      try {
-          const product = await Product.find({_id: req.params.id });
+          const id = req.params.id;
+          const product = await Product.findOne({_id: id }).lean();
           if (!product)
           {
-               res.status(400).json({ message: "Product id does not exits!" });
+               res.status(400).json({ message: `Product ${id} does not exits!` });
+               return;
           }
+
           res.status(200).json(product);
      }
      catch (err : any)
      {
           console.error(err);
-          res.status(400).json({ message: err.message });
+          res.statusCode = 503;
           next(err);
      }
 });
 
 // POST
 productsRouter.post("/", async (req: Request, res: Response, next: NextFunction) =>{
-     const { name, price, rating, category, sizes, stock, description } : ProductInterface = req.body;
+     const { name, price, rating, category, sizes, stock, description } : ProductInterface = req?.body;
      try {
-          if (!name || price ) {
-               res.status(400).json({ message : "New Product must have 'name' and 'price' attribute" });
+          if (!name || !price || !sizes || !description || !stock || !category) {
+               res.status(400).json({ message : "New product must have all fields defined!" });
+               return;
           }
+          
           const result = await Product.insertMany([{ name, price, rating, category, sizes, stock, description}], { rawResult: true});
           console.log(result);
           res.status(200).json({message: "Succesfully added new product"});
@@ -61,7 +66,12 @@ productsRouter.put("/:id", async (req: Request, res: Response, next: NextFunctio
      const id = req?.params.id;
      const updateFields: ProductInterface = req?.body;
      try {
-          const product = await Product.findByIdAndUpdate(id, updateFields);
+          const product = await Product.findByIdAndUpdate(id, updateFields).lean();
+          if (!product)
+          {
+               res.status(400).json({ message: `Product ${id} does not exits!` });
+               return;
+          }
           console.log(product);
           res.status(200).json(product);
      }
@@ -69,6 +79,28 @@ productsRouter.put("/:id", async (req: Request, res: Response, next: NextFunctio
      {
           console.error(err);
           res.status(503).json({message: err.message});
+          next(err);
+     }
+});
+
+
+// DELETE
+productsRouter.delete("/:id", async(req: Request, res: Response, next: NextFunction) => {
+     const id = req.params.id;
+     try {
+          const { deletedCount } = await Product.deleteOne({_id: id }).lean();
+          
+          if (deletedCount == 0)
+          {
+               res.status(400).json({ message: `Product ${id} does not exits!` });
+               return;
+          }
+          res.status(200).json({ message : `Product ${id} deleted!`});
+     }
+     catch (err: any)
+     {
+          console.error(err);
+          res.status(503).json({ message: err.message });
           next(err);
      }
 });
