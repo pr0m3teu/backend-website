d5a6c9a02f601627a57f6e77d811c235dae3e91f
Mihai Andrei <andreimihai2305is@gmail.com>
2024-12-17 22:03:57 +0200

    added refresh auth route

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -3,8 +3,33 @@ import User from "../models/User";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { UserInterface } from "../interfaces";
 
 dotenv.config();
+
+function generateToken(user : { email: string, admin : boolean }, expiresIn: string)
+{
+    // Chech if .env contains the token secrets
+    if (!process.env.ACCESS_TOKEN_SECRET) {
+        throw new Error("There is no access token for loging in users!");
+    } 
+    else if (!process.env.REFRESH_TOKEN_SECRET) {
+        throw new Error("There is not refresh token for loging in users!");
+    }
+    
+    const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "email": user.email,
+                "admin": user.admin
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn }
+    );
+
+    return accessToken;
+}
 
 export async function login(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req?.body;
@@ -37,27 +62,8 @@ export async function login(req: Request, res: Response, next: NextFunction) {
         }
 
         // User JWT tokens
-        const accessToken = jwt.sign(
-            {
-                "UserInfo": {
-                    "email": user.email,
-                    "admin": user.admin
-                }
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: "1h"}
-        );
-
-        const refreshToken = jwt.sign(
-            {
-                "UserInfo": {
-                    "email": user.email,
-                    "admin": user.admin
-                }
-            },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: "10d" }
-        )
+        const accessToken = generateToken(user, "1h");
+        const refreshToken = generateToken(user, "1d");
 
         // Create secure cookie
         res.cookie('jwt', refreshToken, {
@@ -76,7 +82,40 @@ export async function login(req: Request, res: Response, next: NextFunction) {
 }
 
 export async function refresh(req: Request, res: Response, next: NextFunction) {
-    res.status(404);
+    const cookies = req.cookies;
+    if (!cookies?.jwt) {
+        res.sendStatus(401);
+    }
+
+    try {
+        // Chech if .env contains the token secrets
+        if (!process.env.ACCESS_TOKEN_SECRET) {
+            throw new Error("There is no access token for loging in users!");
+        } 
+        else if (!process.env.REFRESH_TOKEN_SECRET) {
+            throw new Error("There is not refresh token for loging in users!");
+        }
+
+        const refreshToken = cookies.jwt;
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err : any, user : any) => {
+            if (err) {
+                res.sendStatus(403);
+                next(err);
+            }
+
+            const foundUser = await User.findOne({ email: user?.UserInfo?.email }).exec();
+            if (!foundUser) 
+            {
+                res.sendStatus(401);
+            }
+            
+            const newAccessToken = generateToken({ email: foundUser?.email!, admin: foundUser?.admin!}, "1h");
+            res.json({ accessToken: newAccessToken });
+        });
+    
+    } catch (err: any) {
+        res.status(500).json({ message: err.message });
+    }
 }
 
 export async function logout(req: Request, res: Response, next: NextFunction) {
